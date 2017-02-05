9ab3fa3d9d13290fc23a5d1e62e7f0149790d471
Jesse Harris <jrharris@gmail.com>
2017-02-05 14:21:55 +0700

    Initial upload of files for plot2

---
+++ plot2.R
@@ -0,0 +1,16 @@
+# The household_power_consumption.txt file should be in your R working directory
+# in order for the code below to work
+
+# Read the file into R
+power <- read.table(file = "household_power_consumption.txt", na.strings = "?", header = TRUE, sep = ";")
+
+# Combine/convert date and time readings
+power$Timestamp <- with(power, as.POSIXct(paste(Date, Time, sep = " "), format = "%d/%m/%Y %H:%M:%S"))
+
+# Subset for desired date range (Feb 1 & 2, 2007)
+subset <- subset(power, Timestamp >= as.POSIXct("2007-02-01") & Timestamp < as.POSIXct("2007-02-03"), select = -(c(Date, Time)))
+
+# Create plot in PNG format
+png(filename = "plot2.png", width = 480, height = 480, units = "px", bg = "transparent")
+plot(subset$Timestamp, subset$Global_active_power, type = "l", col = "black", ylab = "Global Active Power (kilowatts)", xlab = "")
+dev.off()
